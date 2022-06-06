19ebc8e903878259784644233e212b2bf3009b69
Prosvetova Lina <linchik@mail.ru>
2022-06-06 15:20:03 +0300

    задержка AsyncAfter

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+        
     }
-    */
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+        }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+        
+        fileprivate func loginAlert() {
+            let ac = UIAlertController(title: "Зарегистрированы?", message: "Введите ваш логин и пароль", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            let cancelAction = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+            
+            ac.addAction(okAction)
+            ac.addAction(cancelAction)
+            
+            ac.addTextField { (usernameTF) in
+                usernameTF.placeholder = "Введите логин"
+            }
+            
+            ac.addTextField { (userPasswordTF) in
+                userPasswordTF.placeholder = "Введите пароль"
+                userPasswordTF.isSecureTextEntry = true
+            }
+            
+            self.present(ac, animated: true, completion: nil)
+        }
+        
+    
 
 }
